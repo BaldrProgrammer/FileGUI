02e902c5fac760bd3975b6ce5cf997986229a895
BaldrProgrammer <boliklevik@gmail.com>
2025-12-30 00:15:19 +0100

    RenamePopup Window Form

--- FileGUI/Popups/RenamePopup.cs
+++ FileGUI/Popups/RenamePopup.cs
@@ -8,6 +8,31 @@
 
     public RenamePopup()
     {
+        label = new Label();
+        label.Text = "Provide new name";
+        label.Location = new Point(50, 10);
+        label.Font = new Font("Times New Roman", 12);
+        label.AutoSize = true;
         
+        textBox = new TextBox();
+        textBox.Location = new Point(50, 60);
+        textBox.Size = new Size(175, 30);
+        textBox.Font = new Font("Times New Roman", 12);
+        
+        submit = new Button();
+        submit.Text = "OK";
+        submit.Location = new Point(75, 100);
+        submit.Size = new Size(125, 30);
+        submit.Font = new Font("Times New Roman", 12);
+        
+        submit.Click += (s, e) => Close();
+        
+        Controls.AddRange(
+            label, textBox, submit
+        );
+        
+        Size = new Size(300, 200
+        );
+        Deactivate += (s, e) => Close();
     }
 }
