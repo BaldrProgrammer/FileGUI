05f012e1f9f6c3f5e0de515a004e1936c6eae740
BaldrProgrammer <boliklevik@gmail.com>
2026-01-07 13:38:57 +0100

    menu button create file

--- FileGUI/obj/Debug/net10.0-windows/FileGUI.AssemblyInfo.cs
+++ FileGUI/obj/Debug/net10.0-windows/FileGUI.AssemblyInfo.cs
@@ -13,12 +13,12 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a8e066e76d63b9c89d03e3fefc8e679ef5ba7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10cfb4bdcf21582a4804a14f75441cd06c997dc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
-// Создано классом WriteCodeFragment MSBuild.
+// Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
 
